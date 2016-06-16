8f50c1957ad1740b17e55c290b6087671d6bddb7
Gleibson Silva <gleibsongyn@gmail.com>
2016-06-16 20:10:49 -0300

    Initial commit

---
+++ app/src/main/java/com/ufg/gleibson/postos/Controler/InicioActivity.java
@@ -0,0 +1,15 @@
+package com.ufg.gleibson.postos.Controler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ufg.gleibson.postos.R;
+
+public class InicioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+    }
+}
---
+++ app/src/main/java/com/ufg/gleibson/postos/Model/Usuario.java
@@ -0,0 +1,25 @@
+package com.ufg.gleibson.postos.Model;
+
+/**
+ * Created by gleibson on 16/06/16.
+ */
+public class Usuario {
+    private String nome;
+    private String nomeUsuario;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+}
