d51f0718ee1c72cac21bfbd2ae78f40a1590110a
Gleibson Silva <gleibsongyn@gmail.com>
2016-06-16 21:07:13 -0300

    Otimização de SplashScreen.

--- app/src/main/java/com/ufg/gleibson/postos/Controler/Splash.java
+++ app/src/main/java/com/ufg/gleibson/postos/Controler/Splash.java
@@ -14,7 +14,6 @@ public class Splash extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
 
         new Handler().postDelayed(new Runnable() {
             @Override
