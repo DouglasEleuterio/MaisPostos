1cbf8b5e02f65fe55b14950290cfcc68c9c56f6c
Gleibson Silva <gleibsongyn@gmail.com>
2016-06-16 21:51:36 -0300

    Otimização de SplashScreen.

--- app/src/main/java/com/ufg/gleibson/postos/Controler/InicioActivity.java
+++ app/src/main/java/com/ufg/gleibson/postos/Controler/InicioActivity.java
@@ -3,8 +3,6 @@ package com.ufg.gleibson.postos.Controler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.ufg.gleibson.postos.R;
-
 public class InicioActivity extends AppCompatActivity {
 
     @Override
--- app/src/main/java/com/ufg/gleibson/postos/Controler/SplashActivity.java
+++ app/src/main/java/com/ufg/gleibson/postos/Controler/SplashActivity.java
@@ -5,8 +5,6 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.ufg.gleibson.postos.R;
-
 public class SplashActivity extends AppCompatActivity {
 
     @Override
