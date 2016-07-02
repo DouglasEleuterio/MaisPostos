61ea0a6fe044e7d694d61dafa5a1c3dc3b3d69eb
Gleibson Silva <gleibsongyn@gmail.com>
2016-07-01 21:40:54 -0300

    Atualizações em todas as views.

--- app/src/main/java/com/ufg/gleibson/postos/Controler/InicioActivity.java
+++ app/src/main/java/com/ufg/gleibson/postos/Controler/InicioActivity.java
@@ -1,7 +1,15 @@
 package com.ufg.gleibson.postos.Controler;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
 
 import com.ufg.gleibson.postos.R;
 
@@ -11,5 +19,35 @@ public class InicioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+    }
+
+    public void novoPosto(View view){
+        Intent intent = new Intent(this, NovoPostoActivity.class);
+        startActivity(intent);
+    }
+    public void cadastro(View view) {
+        Intent intent = new Intent(this, CadastroActivity.class);
+        startActivity(intent);
+    }
+    public void login(View view){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+    public void posto(View view){
+        Intent intent = new Intent(this, PostoActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+        return true;
     }
 }
